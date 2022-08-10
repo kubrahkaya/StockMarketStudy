e98987f691be755e08e65eeccf9d51711de77322
Kubra Harmankaya <kubra.harmankaya@ly.st>
2022-08-10 18:48:38 +0100

    Initial commit

---
+++ app/src/main/java/com/innovaocean/stockmarketstudy/StockApplication.kt
@@ -0,0 +1,7 @@
+package com.innovaocean.stockmarketstudy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StockApplication: Application()
---
+++ app/src/main/java/com/innovaocean/stockmarketstudy/ui/Theme.kt
@@ -0,0 +1,29 @@
+package com.innovaocean.stockmarketstudy.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+private val DarkColorPalette = darkColors(
+    primary = Color.Green,
+    background = DarkBlue,
+    onPrimary = Color.DarkGray,
+    onBackground = TextWhite
+)
+
+@Composable
+fun StockMarketAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
