9765632bfb5c0cb1634a1a8c17bac71d65702be2
Kubra Harmankaya <kubra.harmankaya@ly.st>
2022-08-10 19:01:07 +0100

    Di applied on AppModule

---
+++ app/src/main/java/com/innovaocean/stockmarketstudy/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.innovaocean.stockmarketstudy.di
+
+import android.app.Application
+import androidx.room.Room
+import com.innovaocean.stockmarketstudy.data.local.StockDatabase
+import com.innovaocean.stockmarketstudy.data.remote.StockApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(StockApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideStockDatabase(app: Application): StockDatabase {
+        return Room.databaseBuilder(
+            app,
+            StockDatabase::class.java,
+            "stockdb.db"
+        ).build()
+    }
+}
