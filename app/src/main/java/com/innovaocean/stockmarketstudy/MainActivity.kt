faee265089d715f8608ff20361c3ac0ba89a5f02
Kubra Harmankaya <kubra.harmankaya@ly.st>
2022-08-16 09:40:56 +0100

    ui composables created

--- app/src/main/java/com/innovaocean/stockmarketstudy/MainActivity.kt
+++ app/src/main/java/com/innovaocean/stockmarketstudy/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.innovaocean.stockmarketstudy.presentation.companyListings.NavGraphs
 import com.innovaocean.stockmarketstudy.ui.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
             StockMarketAppTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background){
-                   // DestinationsNavHost(navGraph = NavGraphs.root)
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
---
+++ app/src/main/java/com/innovaocean/stockmarketstudy/presentation/companyListings/CompanyItem.kt
@@ -0,0 +1,58 @@
+package com.innovaocean.stockmarketstudy.presentation.companyListings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.innovaocean.stockmarketstudy.domain.model.CompanyListing
+import androidx.compose.material.Text
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Light
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CompanyItem(
+    company: CompanyListing, modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = company.name,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = company.exchange,
+                    fontWeight = Light,
+                    color = MaterialTheme.colors.onBackground
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "(${company.symbol})",
+                fontStyle = FontStyle.Italic,
+                color = MaterialTheme.colors.onBackground
+            )
+        }
+    }
+}
