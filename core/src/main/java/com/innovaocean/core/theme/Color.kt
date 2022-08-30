24d30cc0694a53d3f15f434239165015c542d756
Kubra Harmankaya <kubra.harmankaya@ly.st>
2022-08-30 15:41:29 +0100

    refactor on theming, dark, light colors.

--- core/src/main/java/com/innovaocean/core/theme/Color.kt
+++ core/src/main/java/com/innovaocean/core/theme/Color.kt
@@ -1,6 +1,35 @@
 package com.innovaocean.core.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.ui.graphics.Color
 
-val DarkBlue = Color(0xFF060D2E)
-val TextWhite = Color(0xFFEEEEEE)
+val teal200 = Color(0xFF03DAC5)
+val teal700 = Color(0xFF018786)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+
+ val DarkColorPalette = darkColors(
+    primary = black,
+    primaryVariant = black,
+    onPrimary = white,
+    secondary = teal200,
+    secondaryVariant = teal200,
+    onSecondary = black,
+    surface = black,
+    onSurface = white,
+    background = black,
+    onBackground = white,
+)
+
+ val LightColorPalette = darkColors(
+    primary = black,
+    primaryVariant = black,
+    onPrimary = white,
+    secondary = teal200,
+    secondaryVariant = teal700,
+    onSecondary = black,
+    surface = white,
+    onSurface = black,
+    background = white,
+    onBackground = black,
+)
--- core/src/main/java/com/innovaocean/core/theme/Theme.kt
+++ core/src/main/java/com/innovaocean/core/theme/Theme.kt
@@ -1,28 +1,22 @@
 package com.innovaocean.core.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-
-private val DarkColorPalette = darkColors(
-    primary = Color.Green,
-    background = DarkBlue,
-    onPrimary = Color.DarkGray,
-    onBackground = TextWhite
-)
 
 @Composable
 fun StockMarketAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isSystemInDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = DarkColorPalette,
+        colors = getColors(isSystemInDarkTheme),
         typography = Typography,
-        shapes = Shapes,
         content = content
     )
+}
+
+internal fun getColors(isSystemInDarkTheme: Boolean): Colors {
+    return if (isSystemInDarkTheme) DarkColorPalette else LightColorPalette
 }
